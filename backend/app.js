54e21541cb20dadb19f1235e4346fd08f8279e8f
Phill-For-Real <99283201+Phill-For-Real@users.noreply.github.com>
2023-02-08 11:52:19 +0000

    fixed spelling

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,30 @@
 // imported modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv/config');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
 const urlPrefix = process.env.API_URL;
-const authJwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
 
 //CORS usage and options
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 //middleware
 app.use(express.json());
-app.use(morgan('tiny'));
-app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(morgan("tiny"));
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(authJwt());
 app.use(errorHandler);
 
 //routers
-const productsRouter = require('./routers/products');
-const categoriesRouter = require('./routers/categories');
-const ordersRouter = require('./routers/orders');
-const usersRouter = require('./routers/users');
+const productsRouter = require("./routers/products");
+const categoriesRouter = require("./routers/categories");
+const ordersRouter = require("./routers/orders");
+const usersRouter = require("./routers/users");
 app.use(`${urlPrefix}/products`, productsRouter);
 app.use(`${urlPrefix}/categories`, categoriesRouter);
 app.use(`${urlPrefix}/orders`, ordersRouter);
@@ -36,15 +36,15 @@ mongoose
     dbName: process.env.DB_NAME,
   })
   .then(() => {
-    console.log('DATABASE connection is ready');
+    console.log("DATABASE connection is ready");
   })
   .catch((error) => {
     console.log(error);
   });
 
-const PORT = proces.env.PORT;
+const PORT = process.env.PORT;
 
 // listeners
 app.listen(PORT, () => {
-  console.log('App has started listening');
+  console.log("App has started listening");
 });
